b0aa631e97687719390d5d9fd18e26ed3441737b
Azhan <fe4rtoxin45@gmail.com>
2026-09-18 22:11:59 +0500

    Implement the isSorted() function

--- sorted_array_test.cpp
+++ sorted_array_test.cpp
@@ -106,3 +106,13 @@ int main() {
     cout << "\nAll tests passed!\n";
     return 0;
 }
+
+// Function defination 
+bool isSorted(const int* arr, const int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
